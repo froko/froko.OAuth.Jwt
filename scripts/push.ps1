edd1f8b3294e6fd7eacfecbeefd8ac1687c964dc
Patrick Ineichen <github@frokonet.ch>
2016-10-22 14:37:04 +0200

    Fixed copy-paste-error in push.ps1

--- scripts/push.ps1
+++ scripts/push.ps1
@@ -12,5 +12,5 @@ Foreach-Object {
 	$package = $_.fullname
 	
 	Write-Host "pushing nuget package" $package
-	& cmd /c "$nugetConsole push $package -Source https://www.myget.org/F/froko/api/v2/package"
+	& cmd /c "$nugetCommand push $package -Source https://www.myget.org/F/froko/api/v2/package"
 }
